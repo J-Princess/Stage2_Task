962fe7c1e93a69b1a13a03b607db26ad5bddd270
Oyibo Ufuoma <princessoyibo@gmail.com>
2023-09-15 22:26:15 +0100

    Added Necessary Dets

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -2,6 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import MovieList from './components/MovieList';
 import './index.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Mainbar from './components/MainBar';
+
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <div className="user-container" >
+      <h2>An Error Has Occurred somewhere in your code</h2>
+      <p>{error.message}</p>
+      <button onClick={() => resetErrorBoundary}>Go Home</button>
+    
+    </div>
+  );
+};
+
 
 function App() {
   return (
@@ -20,7 +36,18 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <MovieList />
+      {/* <MovieList /> */}
+   
+      <Routes>
+
+        <Route path='/' element={<MovieList />} />
+    {/* <Route path="/movie/:id">
+          <Mainbar />
+        </Route> */}
+        <Route path="/movie/:id"  element={<Mainbar />} />
+        {/* Add more routes as needed */}
+
+    </Routes>
     </div>
   );
 }
--- movie-app/src/components/MovieUnit.jsx
+++ movie-app/src/components/MovieUnit.jsx
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import IMDPImg from "./images/imbp.png";
+import TomatoImg from "./images/image (1).webp";
+import LoveImg from "./images/Favorite.svg";
 
 const MovieUnit = ({ movie }) => {
-  const [movieImages, setMovieImages] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchMovieImages = async () => {
-  //     const apiKey = '27379da32e0ad1910ce97a137ffcc04b'; // Replace with your TMDB API key
-  //     const movieId = movie.id; // Use the actual movie ID
-
-  //     try {
-  //       const response = await fetch(
-  //         `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=${apiKey}`
-  //       );
-
-  //       if (response.ok) {
-  //         const data = await response.json();
-  //         // Assuming you want to extract images from the response data
-  //         setMovieImages(data.backdrops);
-  //       } else {
-  //         console.error('Failed to fetch movie images');
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching movie images:', error);
-  //     }
-  //   };
-
-  //   fetchMovieImages();
-  // }, [movie.id]);
-
   return (
-    <div className="movie-card" data-testid="movie-card">
-      <img
-        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-        alt={movie.title}
-        data-testid="movie-poster"
-      />
-      <h2 data-testid="movie-title">{movie.title}</h2>
-      <p data-testid="movie-release-date">{movie.release_date}</p>
-
-      {/* Display fetched movie images */}
-      {movieImages.map((image) => (
+    <div className="card p-2 text-start m-3" style={{ width: "18rem",  }} data-testid="movie-card">
+      <Link to={`/movie/${movie.id}`}>
         <img
-          key={image.file_path}
-          src={`https://image.tmdb.org/t/p/w300${image.file_path}`}
-          alt={`Image for ${movie.title}`}
+          src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+          className="card-img-top wow"
+          alt={movie.title}
+          data-testid="movie-poster"
         />
-      ))}
+        <img
+          src={LoveImg}
+          className="fav"
+          alt="Favourite"
+          data-testid="movie-poster"
+          style={{ width: "6rem",  }}
+        />
+      </Link>
+      <div className="card-body">
+      <p className="card-text" data-testid="movie-release-date">
+          {movie.origin_country} {movie.release_date}
+        </p>
+        <h2 className="card-title" data-testid="movie-title">
+          {movie.title}
+        </h2>
+        <div className="d-flex my-4">
+          <img
+            src={IMDPImg}
+            className=""
+            alt="..."
+            style={{ width: "35px", height: "17px" }}
+          />
+          <p style={{ margin: "-4px 13px 0px" }}>{movie.popularity} / 100</p>
+          <img
+            src={TomatoImg}
+            className=""
+            alt="..."
+            style={{ width: "16px", height: "17px" }}
+          />
+          <p style={{ margin: "-4px 10px 0px" }}>{movie.popularity}%</p>
+        </div>
+        <p className="text-dark">{movie.tagline}</p>
+      </div>
     </div>
   );
 };
-
 
 export default MovieUnit;
